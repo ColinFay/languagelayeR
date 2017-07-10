e49d69cece8b8b8ae05c949e03aca6fcf051b6e6
ColinFay <contact@colinfay.me>
2017-07-10 21:24:22 +0200

    Now deprecated

--- R/setApiKey.R
+++ R/setApiKey.R
@@ -4,12 +4,9 @@
 #'
 #'Takes a character string containing your API key.
 #'@param apikey You can find your API key on your dashboard at \url{https://languagelayer.com/dashboard}.
-#'@export
-#'@examples
-#'setApiKey(apikey = "yourapikey")
 
 setApiKey <- function(apikey){
-  options(apikey = apikey)
+  message("This function is deprecated.")
 }
 
 
